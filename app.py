1f179925c5526d59f9a8cac5550c206abbae524e
ETIINLPT <134976239+AdminPublicTest@users.noreply.github.com>
2026-06-03 20:01:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,20 @@ st.sidebar.title("Parametros")
 st.image("Python_logo.png", width = 500)
 st.sidebar.image("DMC.png", width = 300)
 
-st.write("Elaborado por: Carlos Carrillo")
+st.write("Elaborado por: Chris Perea")
 
 archivo = st.file_uploader("Cargue el archivo excel o csv")
+
+if archivo is not None :
+  if archivo.name.endswith(".csv"):
+    data = pd.read_csv(archivo)
+    st.write(data)
+  elif archivo.name.endswith(".xlsx")
+   data = pd.read_excel(archivo)
+else:
+  st.write("Formato no Válido")
+
+else:
+ st.write("Por favor cargue su archivo")
+
+
