7cbb7c97f6af5cb7468c19201a4796d9e9aa4048
ETIINLPT <134976239+AdminPublicTest@users.noreply.github.com>
2026-06-03 19:46:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,5 @@ st.image("Python_logo.png", width = 500)
 st.sidebar.image("DMC.png", width = 300)
 
 st.write("Elaborado por: Carlos Carrillo")
+
+archivo - st.file_uploader("Cargue el archivo excel o csv")
